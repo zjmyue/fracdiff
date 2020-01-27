fb5b197bf09664d05b5fc7b2d9f69f5a4cf64eee
Shota Imaki <shota.imaki@icloud.com>
2020-01-27 11:48:52 +0900

    add param check

--- tests/test_params.py
+++ tests/test_params.py
@@ -28,6 +28,13 @@ invalid_methods = ['hoge']
 #         Fracdiff(window=window).transform(X)
 
 
+def test_fracdiff_noparams():
+    X = np.random.randn(100)
+    with pytest.raises(ValueError):
+        fracdiff = Fracdiff(window=None, tol_coef=None, tol_memory=None)
+        fracdiff.transform(X)
+
+
 @pytest.mark.parametrize('method', invalid_methods)
 def test_stat_method(method):
     X = np.random.randn(100)
