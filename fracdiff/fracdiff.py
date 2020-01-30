249feceaf53cd2c3d2b562e329d21dca7148696d
Shota Imaki <shota.imaki@icloud.com>
2020-01-30 22:29:08 +0900

    streamline computation

--- fracdiff/fracdiff.py
+++ fracdiff/fracdiff.py
@@ -1,5 +1,9 @@
-from sklearn.utils.validation import check_array
+from functools import partial
+
+from bisect import bisect
 import numpy as np
+from sklearn.utils.validation import check_array
+from scipy.special import binom
 
 
 class Fracdiff:
@@ -8,7 +12,7 @@ class Fracdiff:
 
     Parameters
     ----------
-    - order : float, default 1.0
+    - d : float, default 1.0
         Order of differentiation.
     - window : int > 0 or None, default 10
         Minimum number of observations to evaluate each term in fracdiff.
@@ -25,7 +29,7 @@ class Fracdiff:
         absolute value of `window + 1`th fracdiff coefficient is smaller
         than `tol_memory`.
         If `window` is specified, ignored.
-    - window_policy : {'fixed', 'expanding'}, default 'fixed'
+    - window_policy : {'fixed'}, default 'fixed'
         If fixed :
             Each term in fracdiff time-series will be evaluated using
             `window` observations.
@@ -34,38 +38,38 @@ class Fracdiff:
             truncated up to the `window`th term.
             The beginning `window - 1` terms in fracdiff time-series will be
             filled with `numpy.nan`.
-        If expanding :
+        If expanding (not available yet) :
             Each term in fracdiff time-series will be evaluated using at least
             `window` observations.
             The beginning `window - 1` terms in fracdiff time-series will be
             filled with `numpy.nan`.
 
+    Attributes
+    ----------
+    - coef : array, shape (window, )
+        Sequence of coefficients in fracdiff operator.
+
     Examples
     --------
     >>> fracdiff = Fracdiff(0.5)
-    >>> X = np.array([1., 0., 0., 0., 0., 0.]).reshape(-1, 1)
-    >>> Xd = fracdiff.transform(X)
-    >>> Xd
-    array([[ 1.        ],
-           [-0.5       ],
-           [-0.125     ],
-           [-0.0625    ],
-           [-0.0390625 ],
-           [-0.02734375]])
+    >>> X = ...
+    >>> fracdiff.transform(X)
+    ...
+    >>> fracdiff.coef
+    ...
     """
-
-    MAX_WINDOW = 10 ** 6  # upper limit of `self.window`.
+    MAX_WINDOW = 2 ** 20  # upper limit of `self.window`.
 
     def __init__(
         self,
-        order=1.0,
+        d=1.0,
         window=10,
         tol_memory=None,
         tol_coef=None,
         window_policy='fixed',
     ):
         """Initialize self."""
-        self.order = order
+        self.d = d
         self.window = window
         self.tol_memory = tol_memory
         self.tol_coef = tol_coef
@@ -77,71 +81,25 @@ class Fracdiff:
 
         Parameters
         ----------
-        - X : array-like, shape (n_samples, n_features)
+        - X : array-like, shape (n_samples, n_series)
             Time-series to perform fractional differentiation.
 
         Returns
         -------
-        - X_d : array-like, shape (n_samples, n_features)
+        - X_d : array, shape (n_samples, n_series)
             Fractionally differentiated time-series.
             The beginning `self.window - 1` terms will be filled with
             `numpy.nan`.
         """
         X = check_array(X, estimator=self)
         self._fit()
+        n_samples, n_series = X.shape
 
-        n_samples, n_features = X.shape
+        D = partial(np.convolve, self.coef, mode='valid')
+        Xd = np.apply_along_axis(D, 0, X)
 
-        if n_features > 1:
-            return np.hstack([
-                self.transform(X[:, :1]),
-                self.transform(X[:, 1:]),
-            ])
-
-        # Note on implementation:
-        #
-        # For window = 4, n_samples = 5, n_features = 1,
-        #
-        # d = [c_3  c_2  c_1  c_0  0.0  0.0  0.0  0.0]
-        #      <---- window ---->  <- n_samples - 1 ->
-        #
-        # D = [
-        #     [c_0  0.0  0.0  0.0  0.0  c_3  c_2  c_1] >
-        #     [c_1  c_0  0.0  0.0  0.0  0.0  c_3  c_2] |
-        #     [c_2  c_1  c_0  0.0  0.0  0.0  0.0  c_3] |
-        #     [c_3  c_2  c_1  c_0  0.0  0.0  0.0  0.0] window+ n_samples - 1
-        #     [0.0  c_3  c_2  c_1  c_0  0.0  0.0  0.0] |
-        #     [0.0  0.0  c_3  c_2  c_1  c_0  0.0  0.0] |
-        #     [0.0  0.0  0.0  c_3  c_2  c_0  c_0  0.0] |
-        #     [0.0  0.0  0.0  0.0  c_3  c_0  c_0  c_0] >
-        # ]    <------ window + n_samples - 1 ------>
-        #
-        # X_extended
-        #   = [
-        #     [0.0  0.0  0.0  X_0  X_1  X_2  X_3  X_4]
-        #     ]
-        #     < window - 1 >  <----- n_samples ----->
-        #
-        # Y = D @ X_extended
-        #   = [...  ...  ...  ...  ...  ...  Y_3  Y_4]
-        #                     <--- fracdiff of X --->
-        #
-        # return
-        #   = [               nan  nan  nan  Y_0  Y_1]
-
-        # TODO expanding window method
-
-        d = np.concatenate([self.coef, np.zeros(n_samples - 1)], axis=0)
-        D = np.stack([
-            np.roll(d, shift - self.window + 1)
-            for shift in range(self.window + n_samples - 1)
-        ], axis=0)
-        X_extended = np.concatenate([
-            np.zeros((self.window - 1, 1)), X
-        ], axis=0)
-
-        Xd = (D @ X_extended)[-n_samples:, :]
-        Xd[:self.window - 1, :] = np.nan
+        nans = np.full((n_samples - Xd.shape[0], n_series), np.nan)
+        Xd = np.concatenate([nans, Xd], axis=0)
 
         return Xd
 
@@ -149,78 +107,57 @@ class Fracdiff:
         """
         Set `self.window` and `self.coef`.
         """
-        # TODO Simplify logic
-        determine_window = (
-            self.window is None
-            or getattr(self, '_cached_order', self.order)
-            != self.order
-            or getattr(self, '_cached_tol_memory', self.tol_memory)
-            != self.tol_memory
-            or getattr(self, '_cached_tol_coef', self.tol_coef)
-            != self.tol_coef
-        )
-        determine_coef = (
-            not hasattr(self, 'coef')
-            or getattr(self, '_cached_order', self.order)
-            != self.order
-            or getattr(self, '_cached_window', self.window)
-            != self.window
-        )
-        if not (determine_window or determine_coef):
-            return
+        # If parameters have been changed, reset window
+        if self.d < 0.0:
+            raise ValueError('d should be positive.')
 
-        if determine_window:
+        if (
+            getattr(self, '_d', self.d) != self.d
+            or getattr(self, '_tol_memory', self.tol_memory) != self.tol_memory
+            or getattr(self, '_tol_coef', self.tol_coef) != self.tol_coef
+        ):
             self.window = None
 
-        if self.window is not None:
-            self.coef = self._make_coef(window=self.window)
-        elif self.tol_coef is not None:
-            self.coef = self._make_coef(tol_coef=self.tol_coef)
-            self.window = len(self.coef)
-        elif self.tol_memory is not None:
-            self.coef = self._make_coef(tol_memory=self.tol_memory)
-            self.window = len(self.coef)
-        else:
-            raise ValueError('None of window, tol_coef, tol_memory are given.')
+        if self.window is None or not hasattr(self, 'coef'):
+            self.coef = self._get_coef()
+            self._d = self.d
+            self._tol_memory = self.tol_memory
+            self._tol_coef = self.tol_coef
+            self._window = self.window
 
-        self._cached_order = self.order
-        self._cached_tol_memory = self.tol_memory
-        self._cached_tol_coef = self.tol_coef
-        self._cached_window = self.window
+        return self
 
-    def _make_coef(self, window=None, tol_coef=None, tol_memory=None):
+    def _get_coef(self):
         """
-        Generator of coefficients.
-
-        Parameters
-        ----------
-        - window :
-            If specified, truncate up to this number of terms.
-        - tol_coef : float in [0, 1]
-        - tol_memory : float in [0, 1]
+        Return array of coefficients.
 
         Note
         ----
-        Sequence of coefficients add up to 0.
+        The k-th coefficient (k = 0, 1, 2, ...) is given by ::
+            ((-1) ** k) * poch_down(d, k) / k!
         """
-        window = window or self.MAX_WINDOW
-        tol_coef = tol_coef or -np.inf
-        tol_memory = tol_memory or -np.inf
+        # Compute
+        n_terms = self.window or self.MAX_WINDOW
+        k = np.arange(n_terms, dtype=np.float64)
+        s = np.tile([1.0, -1.0], -(-n_terms // 2))[:n_terms]
+        coef = s * binom(self.d, k)
 
-        index = 0
-        coef = 1.0
-        lost_memory = 1.0
+        # Truncate
+        if self.window is None:
+            if self.tol_memory is None and self.tol_coef is None:
+                raise ValueError(
+                    'None of window, tol_coef and tol_memory are specified.'
+                )
+            # FIXME works only for 0 < d < 1
+            lost_memory = 1.0 - np.cumsum(self.coef)
+            window = max(
+                bisect(-lost_memory, -self.tol_memory or np.inf),
+                bisect(-self.coef, -self.tol_coef or np.inf),
+            )
+            self.window = window
+            coef = coef[:self.window]
 
-        coefs = []
+        if self.window < 1:
+            raise ValueError('window should be positive.')
 
-        while (
-            index < window
-            and abs(coef) > tol_coef
-            and lost_memory > tol_memory
-        ):
-            coefs.append(coef)
-            coef *= (index - self.order) / (index + 1)
-            index += 1
-            lost_memory -= abs(coef)  # What if order < 0 ?
-
-        return np.flip(np.array(coefs))
+        return coef
