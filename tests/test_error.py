ae80773506d186760a8d09bb74c5b11a9086159e
Shota Imaki <shota.imaki@icloud.com>
2020-01-30 23:50:53 +0900

    add test_error

--- tests/test_error.py
+++ tests/test_error.py
@@ -9,6 +9,8 @@ from fracdiff._stat import StationarityTester
 params_invalid_d = [-0.1, -1.0]
 params_invalid_window = [0, -10]
 params_invalid_method = ['foo']
+params_invalid_tol_memory = [0.0, -1.0, 2.0]
+params_invalid_tol_coef = [0.0, -1.0, 2.0]
 
 X = np.random.randn(100, 2)
 
@@ -34,6 +36,20 @@ def test_fracdiff_window(window):
 def test_fracdiff_noparams():
     with pytest.raises(ValueError):
         fracdiff = Fracdiff(window=None, tol_coef=None, tol_memory=None)
+        fracdiff.transform(X)
+
+
+@pytest.mark.parametrize('tol_memory', params_invalid_tol_memory)
+def test_fracdiff_tol_memory(tol_memory):
+    with pytest.raises(ValueError):
+        fracdiff = Fracdiff(tol_memory=tol_memory)
+        fracdiff.transform(X)
+
+
+@pytest.mark.parametrize('tol_coef', params_invalid_tol_coef)
+def test_fracdiff_tol_coef(tol_coef):
+    with pytest.raises(ValueError):
+        fracdiff = Fracdiff(tol_coef=tol_coef)
         fracdiff.transform(X)
 
 
